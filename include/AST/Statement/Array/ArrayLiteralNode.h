112aae6cb3d266dd071d9e20ae8873e47472e77e
MrPersonDev <simonashton37@gmail.com>
2023-12-12 19:35:38 -0800

    Add ArrayLiteralNode.h

---
+++ include/AST/Statement/Array/ArrayLiteralNode.h
@@ -0,0 +1,15 @@
+#ifndef SYNTHSCRIPT_ARRAYLITERALNODE_H
+#define SYNTHSCRIPT_ARRAYLITERALNODE_H
+
+#include "AST/ASTNode.h"
+
+class ArrayLiteralNode : public ASTNode {
+public:
+    explicit ArrayLiteralNode(std::vector<ASTNode*> values, int line, int col) : ASTNode(line, col), values(std::move(values)) {}
+    ~ArrayLiteralNode() override = default;
+    std::vector<ASTNode*> *getValues() { return &values; }
+private:
+    std::vector<ASTNode*> values;
+};
+
+#endif //SYNTHSCRIPT_ARRAYLITERALNODE_H
