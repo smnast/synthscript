ea0566463dbf79ddaaae62a6dc2b0a8069528f75
MrPersonDev <simonashton37@gmail.com>
2023-12-12 19:39:58 -0800

    Update NodeForwardClasses.h

--- include/AST/NodeForwardClasses.h
+++ include/AST/NodeForwardClasses.h
@@ -1,27 +1,23 @@
 #ifndef CLIKE_NODEFORWARDCLASSES_H
 #define CLIKE_NODEFORWARDCLASSES_H
 
-class ASTNode;
-class ProgramNode;
-class CompoundStatementNode;
 class BinOpNode;
-class UnaryOpNode;
 class CastOpNode;
 class SubscriptOpNode;
-class ArrayDeclarationStatementNode;
+class UnaryOpNode;
+class ArrayLiteralNode;
 class AssignmentNode;
-class CompoundAssignmentNode;
-class DeclarationNode;
-class InitializationNode;
 class BreakStatementNode;
 class ContinueStatementNode;
-class ReturnStatementNode;
-class IfStatementNode;
-class WhileStatementNode;
 class ForStatementNode;
+class IfStatementNode;
+class ReturnStatementNode;
+class WhileStatementNode;
 class FunctionDeclarationNode;
 class FunctionStatementNode;
+class CompoundStatementNode;
 class IdentifierNode;
 class LiteralNode;
+class ProgramNode;
 
 #endif //CLIKE_NODEFORWARDCLASSES_H
