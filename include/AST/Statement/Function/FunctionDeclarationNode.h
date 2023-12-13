eff721c92f88ed2b875b963461bed164724f4720
MrPersonDev <simonashton37@gmail.com>
2023-12-12 21:01:55 -0800

    Remove return type from FunctionDeclarationNode.h

--- include/AST/Statement/Function/FunctionDeclarationNode.h
+++ include/AST/Statement/Function/FunctionDeclarationNode.h
@@ -8,17 +8,15 @@
 
 class FunctionDeclarationNode : public ASTNode {
 public:
-    FunctionDeclarationNode(Type returnType, ASTNode *identifier, std::vector<ASTNode*> parameters, ASTNode *body, int line, int col) : ASTNode(line, col), returnType(returnType), identifier(identifier), parameters(std::move(parameters)), body(body) {}
+    FunctionDeclarationNode(ASTNode *identifier, std::vector<ASTNode*> parameters, ASTNode *body, int line, int col) : ASTNode(line, col), returnType(returnType), identifier(identifier), parameters(std::move(parameters)), body(body) {}
     ~FunctionDeclarationNode() override {
         delete identifier;
         delete body;
     }
-    Type getReturnType() { return returnType; }
     ASTNode *getIdentifier() { return identifier; }
     std::vector<ASTNode*> *getParameters() { return &parameters; }
     ASTNode *getBody() { return body; }
 private:
-    Type returnType;
     ASTNode *identifier;
     std::vector<ASTNode*> parameters;
     ASTNode *body;
