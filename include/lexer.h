2dbe30bdb920b345715e54d7add2764ff2189699
Simon Ashton <simonashton.dev@gmail.com>
2024-10-16 08:11:37 -0700

    Refactor Lexer class

--- include/lexer.h
+++ include/lexer.h
@@ -5,17 +5,69 @@
 #include "tokens.h"
 #include <regex>
 
+/**
+ * @class Lexer
+ * @brief Provides functions for lexical analysis of the code.
+ */
 class Lexer {
 public:
-    static std::vector<Token> parse_tokens(std::string &file);
+    /**
+     * @brief Parse the input code into tokens.
+     * @param code The code to be parsed.
+     * @return std::vector<Token> 
+     */
+    static std::vector<Token> parse_tokens(std::string &code);
 
 private:
-    static std::vector<int> line_prefix, column_prefix;
+    /**
+     * @brief Stores the line of each character in the code.
+     */
+    static std::vector<int> line_prefix;
+
+    /**
+     * @brief Stores the beginning offset of the line for each character in the code.
+     */
+    static std::vector<int> line_offset_prefix;
+
+    /**
+     * @brief Combines all the regexes into one expression.
+     * @return The full regex expression.
+     */
     static std::string combine_regex();
-    static void prepare_line_prefix(std::string &file);
-    static void prepare_column_prefix(std::string &file);
+
+    /**
+     * @brief Prepare the line and column prefixes.
+     * @param code The code to be parsed.
+     */
+    static void prepare_prefixes(std::string &code);
+
+    /**
+     * @brief Parse the code into tokens (lexical analysis).
+     * @param code The code to be parsed.
+     * @param token_regex The combined regex for all the tokens.
+     */
+    static std::vector<Token> get_tokens(std::string &code, std::string &token_regex);
+
+    /**
+     * @brief Get the line of a character in the code.
+     * @param position The position of the character.
+     * @return The line of the character.
+     */
     static int get_line(int position);
+
+    /**
+     * @brief Get the column of a character in the code.
+     * @param position The position of the character.
+     * @return The column of the character, relative to the line.
+     */
     static int get_column(int position);
+
+    /**
+     * @brief Report an error for an undefined token.
+     * @param token The token that is undefined.
+     * @param line The line of the token.
+     * @param column The column of the token.
+     */
     static void lexer_error(const std::string &token, int line, int column);
 };
 
--- src/lexer.cpp
+++ src/lexer.cpp
@@ -1,48 +1,31 @@
 #include "lexer.h"
 
-std::vector<int> Lexer::line_prefix, Lexer::column_prefix;
+std::vector<int> Lexer::line_prefix, Lexer::line_offset_prefix;
 
-std::vector<Token> Lexer::parse_tokens(std::string &file) {
-    prepare_line_prefix(file);
-    prepare_column_prefix(file);
-
+std::vector<Token> Lexer::parse_tokens(std::string &code) {
+    // Preparation for lexical analysis
+    prepare_prefixes(code);
+    prepare_prefixes(code);
     std::string token_regex = combine_regex();
-    std::vector<Token> tokens;
 
-    std::regex reg(token_regex);
-    std::smatch match;
+    // Lexical analysis
+    std::vector<Token> tokens = get_tokens(code, token_regex);
 
-    auto start = file.cbegin(), end = file.cend();
-    while (regex_search(start, end, match, reg)) {
-        for (int i = 1; i < (int)match.size(); i++) {
-            if (!(int)match[i].str().empty()) {
-                int position = (int)(match[i].second - file.cbegin()) - 1;
-                int line = get_line(position), column = get_column(position);
-                Token cur_token(token_regexs[i - 1].first, match[i].str(), line, column);
-                if (token_regexs[i - 1].first == UNDEFINED) {
-                    Lexer::lexer_error(match[i].str(), line, column);
-                } else if (token_regexs[i - 1].first != ESCAPED_NEW_LINE) {
-                    tokens.push_back(cur_token);
-                }
-
-                start = match[i].second;
-                break;
-            }
-        }
-    }
-
-    Token end_token(END_OF_FILE, "", get_line((int)file.size() - 1), 1);
-    tokens.push_back(end_token);
     return tokens;
 }
 
 std::string Lexer::combine_regex() {
+    // Combine all the regexes into one with the format:
+    // (regex_1)|(regex_2)|...|(regex_N)
     std::string token_regex;
     for (auto &token_expr : token_regexs) {
         token_regex += "(" + token_expr.second + ")";
         token_regex += "|";
     }
+
+    // Remove extra separator ('|')
     token_regex.pop_back();
+
     return token_regex;
 }
 
@@ -50,26 +33,65 @@ void Lexer::lexer_error(const std::string &token, int line, int col) {
     Error::error_at_pos("Undefined token: '" + token + "'", line, col);
 }
 
-void Lexer::prepare_line_prefix(std::string &file) {
-    line_prefix.assign(file.size(), 0);
+void Lexer::prepare_prefixes(std::string &code) {
+    line_prefix.assign(code.size(), 0);
+    line_offset_prefix.assign(code.size(), 0);
+
+    // Prepare the line and line offset prefixes
+    int last_line_start = 0;
     int current_line = 1;
-    for (int i = 0; i < (int)file.size(); i++) {
+    for (int i = 0; i < (int)code.size(); i++) {
+        line_offset_prefix[i] = last_line_start;
         line_prefix[i] = current_line;
-        if (file[i] == '\n') {
+
+        // New line
+        if (code[i] == '\n') {
+            last_line_start = i + 1;
             current_line++;
         }
     }
 }
 
-void Lexer::prepare_column_prefix(std::string &file) {
-    column_prefix.assign(file.size(), 0);
-    int last_line_start = 0;
-    for (int i = 0; i < (int)file.size(); i++) {
-        column_prefix[i] = last_line_start;
-        if (file[i] == '\n') {
-            last_line_start = i + 1;
+std::vector<Token> Lexer::get_tokens(std::string &code, std::string &token_regex) {
+    std::vector<Token> tokens;
+
+    // Prepare regex
+    std::regex reg(token_regex);
+    std::smatch match;
+
+    // Tokenize the code
+    auto start = code.cbegin(), end = code.cend();
+    while (regex_search(start, end, match, reg)) {
+        // First match is the entire regex, the rest are the individual groups
+        for (int i = 1; i < (int)match.size(); i++) {
+            int token_idx = i - 1;
+            bool is_match_empty = match[i].str().empty();
+            if (!is_match_empty) {
+                // Parse the token
+                int position = (int)(match[i].second - code.cbegin()) - 1;
+                int line = get_line(position), column = get_column(position);
+                Token cur_token(token_regexs[token_idx].first, match[i].str(), line, column);
+
+                // Check for undefined tokens
+                if (token_regexs[token_idx].first == UNDEFINED) {
+                    Lexer::lexer_error(match[i].str(), line, column);
+                // Don't include new line escapes in the token list
+                } else if (token_regexs[token_idx].first != ESCAPED_NEW_LINE) {
+                    tokens.push_back(cur_token);
+                }
+
+                // Move the start position to the end of the current match
+                start = match[i].second;
+                break;
+            }
         }
     }
+
+    // Add the end of file token
+    Token end_token(END_OF_FILE, "", get_line((int)code.size() - 1), 1);
+    tokens.push_back(end_token);
+
+    return tokens;
 }
 
 int Lexer::get_line(int position) {
@@ -77,5 +99,5 @@ int Lexer::get_line(int position) {
 }
 
 int Lexer::get_column(int position) {
-    return position - column_prefix[position] + 1;
+    return position - line_offset_prefix[position] + 1;
 }
